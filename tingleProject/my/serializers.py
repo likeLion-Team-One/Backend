a563877c0e666b4ca1249f9c7de9c5cd297f6749
zuyo7852 <leejuyeon7852@gmail.com>
2025-07-20 19:29:33 +0900

    region_display 추가

--- tingleProject/my/serializers.py
+++ tingleProject/my/serializers.py
@@ -1,10 +1,15 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
 from .models import MyProfile
 
 class MyProfileSerializer(ModelSerializer):
+    region =SerializerMethodField()
+
     class Meta:
         model = MyProfile
         fields = [
             'id', 'name', 'age', 'gender', 'region', 'district',
             'job','education', 'major', 'detail',
         ]
+    
+    def get_region(self, obj):
+        return obj.get_region_display()
